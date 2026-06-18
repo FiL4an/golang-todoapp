85a95f867682abfd6f703cca39f7a0066426c08a
afilippov <art.filippok@bk.ru>
2026-06-18 22:00:56 +0300

    fix

--- internal/feature/web/repository/file_system/get_file.go
+++ internal/feature/web/repository/file_system/get_file.go
@@ -1,6 +1,7 @@
 package web_fs_repository
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -10,10 +11,11 @@ import (
 func (r *WebRepository) GetFile(filePath string) ([]byte, error) {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
-		if os.IsNotExist(err) {
+		if errors.Is(err, os.ErrNotExist) {
 			return nil, fmt.Errorf("file:%s: %w ", filePath, core_errors.ErrNotFound)
 		}
 		return nil, fmt.Errorf("Get file: %s, %w ", filePath, err)
 	}
+
 	return file, nil
 }
